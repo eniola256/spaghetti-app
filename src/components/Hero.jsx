a751e8e3e78c8b4fa94d110c9368b9cafe0d8ea2
Eniola <eniolaa177@gmail.com>
2026-08-10 04:04:13 +0100

    new style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,39 @@ import { Link } from 'react-router-dom'
 function Hero() {
   return (
     <section className="hero">
-      <img src="/spag2.png" alt="AE Cups coffee" className="spag-img" />
-      <div className="hero-text">
-        <h1 className="hero-title">Fuck "U" Spag</h1>
-        <p className="hero-subtitle">Dished to perfection</p>
-        <Link to="/order" className="hero-cta">Go to Menu</Link>
+      <div className="hero-col hero-text-col">
+        <h1 className="hero-title">Spag <br /> <span>Zone</span></h1>
+        <p className="hero-subtitle">Deliciousness awaits you <br />just a click away!</p>
+        <Link to="/order" className="hero-cta">Order Now</Link>
+      </div>
+
+      <div className="hero-col hero-image-col">
+        <img src="/spag2.png" alt="Spaghetti" className="spag-img" />
+      </div>
+
+      <div className="hero-col hero-pills-col">
+        <div className="pill pill-active">
+          <img src="/spag2.png" alt="Spaghetti" className="pill-img" />
+          <div className="pill-txt">
+            <span>Spaghetti</span>
+            <span>₦500</span>
+          </div>
+          
+        </div>
+        <div className="pill">
+          <img src="/spag2.png" alt="Spaghetti" className="pill-img" />
+          <div className="pill-txt">
+            <span>Fish</span>
+            <span>₦800</span>
+          </div>
+        </div>
+        <div className="pill">
+          <img src="/spag2.png" alt="Spaghetti" className="pill-img" />
+          <div className="pill-txt">
+            <span>Plantain</span>
+            <span>₦500</span>
+          </div>
+        </div>
       </div>
     </section>
   )
