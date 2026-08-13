f841c4b8ed3f8e345f8b0f3e370b060ed915f8c1
Eniola <eniolaa177@gmail.com>
2026-08-13 12:01:59 +0100

    updated import for navbar css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,8 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext.jsx'
-import './Navbar.css'
+import './NavBar.css'
 
-function Navbar() {
+function NavBar() {
   const { user, loading, logout } = useAuth()
   const navigate = useNavigate()
 
